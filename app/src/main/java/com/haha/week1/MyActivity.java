b40276422c01a1c5bffd5a73564e4a566e22db27
andrewpk95 <andrewpk95@hotmail.com>
2015-12-30 00:23:29 +0900

    발

--- app/src/main/java/com/haha/week1/MyActivity.java
+++ app/src/main/java/com/haha/week1/MyActivity.java
@@ -2,8 +2,6 @@ package com.haha.week1;
 
 import android.content.Intent;
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -23,6 +21,11 @@ import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MyActivity extends AppCompatActivity {
 
